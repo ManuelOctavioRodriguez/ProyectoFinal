1eb825e5889e8045cb652d1fde601c07e3bae91e
Manuel Rodriguez <manueloctaviorodriguez1995@gmail.com>
2023-05-22 09:48:50 -0300

    Mejoras en el template

--- Blog/forms.py
+++ Blog/forms.py
@@ -5,9 +5,3 @@ class FormularioArticulo(forms.ModelForm):
     class Meta:
         model = Articulo
         fields = ('titulo', 'subtitulo', 'cuerpo', 'imagen')
-        widgets = {
-            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
-            'subtitulo': forms.TextInput(attrs={'class': 'form-control'}),
-            'cuerpo': forms.Textarea(attrs={'class': 'form-control'}),
-            'imagen': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
-        }
