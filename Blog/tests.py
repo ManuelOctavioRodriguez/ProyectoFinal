6802d64d01ea8213bf4356355059de7d4f89b494
Manuel Rodriguez <manueloctaviorodriguez1995@gmail.com>
2023-05-22 15:30:30 -0300

    Creación de pruebas

--- Blog/tests.py
+++ Blog/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Perfil, Articulo
 
-# Create your tests here.
+class ArticuloModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create_user(username='usuario_prueba', password='contraseña_prueba')
+        perfil = Perfil.objects.create(user=user, nombre='Autor de prueba')
+
+        Articulo.objects.create(
+            id=1,
+            titulo="Título de prueba",
+            subtitulo="Subtítulo de prueba",
+            cuerpo="Cuerpo del artículo de prueba",
+            autor=perfil,
+            imagen="ruta/de/imagen.jpg"
+        )
+        
+    def test_creacion_articulo(self):
+        articulo = Articulo.objects.get(id=1)
+        self.assertEqual(Articulo.objects.count(), 1)
+        self.assertEqual(articulo.titulo, "Título de prueba")
+        self.assertEqual(articulo.subtitulo, "Subtítulo de prueba")
+        self.assertEqual(articulo.cuerpo, "Cuerpo del artículo de prueba")
+        self.assertEqual(articulo.autor.nombre, "Autor de prueba")
+        self.assertEqual(articulo.imagen, "ruta/de/imagen.jpg")
+
+    def test_articulo_str(self):
+        articulo = Articulo.objects.get(id=1)
+        expected_str = "Título de prueba"
+        self.assertEqual(articulo.__str__(), expected_str)
+
--- Perfiles/tests.py
+++ Perfiles/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Perfil
 
-# Create your tests here.
+class PerfilModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create_user(username='usuario_prueba', password='contraseña_prueba')
+        Perfil.objects.create(user=user, nombre='Nombre de prueba', descripcion='Descripción de prueba')
+
+    def test_relacion_usuario(self):
+        perfil = Perfil.objects.get(id=1)
+        self.assertEqual(perfil.user.username, 'usuario_prueba')
+
+    def test_campos_adicionales(self):
+        perfil = Perfil.objects.get(id=1)
+        self.assertEqual(perfil.nombre, 'Nombre de prueba')
+        self.assertEqual(perfil.descripcion, 'Descripción de prueba')
+
+    def test_metodo_str(self):
+        perfil = Perfil.objects.get(id=1)
+        self.assertEqual(perfil.__str__(), 'usuario_prueba')
+
+    def test_campos_opcionales(self):
+        perfil = Perfil.objects.create(user=User.objects.create_user(username='usuario_prueba2', password='contraseña_prueba2'), nombre='Nombre de prueba 2')
+        self.assertTrue(not perfil.avatar)
+        self.assertEqual(perfil.descripcion, '')
+
